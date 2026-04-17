25c5dd4b0fc1ba10591de7b5277fd280f4786266
irfanard27 <ardiafire27@gmail.com>
2026-04-17 22:23:17 +0700

    initialze project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+import icon from "astro-icon";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [icon()],
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+});
---
+++ src/content.config.ts
@@ -0,0 +1,23 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from "astro:content";
+
+// 2. Import loader(s)
+import { glob } from "astro/loaders";
+
+// 3. Import Zod
+import { z } from "astro/zod";
+
+// 4. Define a `loader` and `schema` for each collection
+const blog = defineCollection({
+  loader: glob({ base: "./src/data/blog", pattern: "**/*.md" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    slug: z.string(),
+    publish_date: z.string(),
+    image: z.string(),
+  }),
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
