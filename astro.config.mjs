38ee915a55fe005c7c0a2461dd56c27b0b12e323
irfanard27 <ardiafire27@gmail.com>
2026-04-20 21:51:01 +0700

    Add functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+  site: 'https://your-domain.vercel.app',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
