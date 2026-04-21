a0a31811bbe2253209009abd1413f1f25e0793c5
irfanard27 <ardiafire27@gmail.com>
2026-04-21 21:20:58 +0700

    add slider

---
+++ src/data/home.ts
@@ -0,0 +1,31 @@
+const sliders = [
+  {
+    header: "Solusi Hukum Terpercaya untuk Perusahaan & Individu",
+    sub_header:
+      "Kami membantu Anda menyelesaikan permasalahan hukum dengan pendekatan strategis, terukur, dan berintegritas tinggi.",
+    cta: "Konsultasi Gratis Sekarang",
+    href: "https://wa.me/62822918789",
+  },
+
+  {
+    header: "Spesialis Pengacara Kepailitan & PKPU dan Bidang Hukum Lainnya",
+    sub_header:
+      "Berpengalaman menangani restrukturisasi utang, PKPU, hingga perkara pailit perusahaan dengan pendekatan strategis.",
+    cta: "Jelajahi  Layanan",
+    href: "/service",
+  },
+
+  {
+    header: "Lindungi Bisnis & Aset Anda Hari Ini",
+    sub_header:
+      "Dapatkan solusi hukum yang terukur dan profesional untuk menjaga stabilitas bisnis Anda. Konsultasikan permasalahan Anda bersama tim kami hari ini.",
+    cta: "Hubungi Kami",
+    href: "https://wa.me/62822918789",
+  },
+];
+
+export const homeData = {
+  hero: {
+    sliders: sliders,
+  },
+};
