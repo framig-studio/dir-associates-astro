8edea8b56e2b40c0f81e4656c3cc8f172530b206
irfanard27 <ardiafire27@gmail.com>
2026-04-22 14:33:10 +0700

    Add review from google

---
+++ src/data/testimonials.ts
@@ -0,0 +1,44 @@
+export interface Testimonial {
+  name: string;
+  date: Date;
+  content: string;
+}
+
+export const testimonials: Testimonial[] = [
+  {
+    name: "Jaya Ibubapak adek2iparcndracuttrrcinta",
+    date: new Date("2025-12-22"),
+    content:
+      "Masalah terselesaikan sampai. Tuntas dengan ketulusan hati beliau membantu yang lemah dan semoga selalu di lancarkan di. Banyakkan rizkinya buat Pak okto dan team . Terimaksih banyak. Salam kenal . God blessing you",
+  },
+  {
+    name: "Alwin Wahyu",
+    date: new Date("2025-1-22"),
+    content:
+      "Sangat membantu saudara saya. Pelayanan sangat baik. Semoga semakin sukses Pengacara Surabaya ( Pak Octo dan staff ) salam sehat TYM",
+  },
+  {
+    name: "Mukhsin Saleh",
+    date: new Date("2025-10-22"),
+    content:
+      "Pak dwi ockto saya kenal sangat baik dan tg jawab dlm hal semua kasus clainnya beliau komitmen dan kinerjanya sangat memuaskan bagi pencari keadilan kelebihan pak octo tdk orientasi ke cuan tat kala membela orang yg lemah bangga indonesia memiliki pengacara yg hebat demi membela yg hak orang2 yg terdzolimi",
+  },
+  {
+    name: "Junias Andrian",
+    date: new Date("2025-10-05"),
+    content:
+      "Pengacara sangat berpengalaman, proses dari awal hingga akhir dibantu dengan mulus dan hasilnya cepat, sangat memuaskan",
+  },
+  {
+    name: "Livia Salim",
+    date: new Date("2025-12-12"),
+    content:
+      "Pak Otto, seorang pengacara yang baik dan bertggjawab, proses lancar dan sukses, terima kasih bnyk Pak Otto dan Team, sukses terus Pak Otto SBY",
+  },
+  {
+    name: "Hubert Jong",
+    date: new Date("2025-12-17"),
+    content:
+      'Pengacara andal, tidak neko" dan sangat membantu selesaikan proses perkara sampai tuntas. Mantull pak dwi',
+  },
+];
