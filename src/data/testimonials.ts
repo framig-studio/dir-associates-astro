367460264c37d5be0a6766e9f743f71ffec6a901
irfanard27 <ardiafire27@gmail.com>
2026-04-22 20:27:06 +0700

    Fix date from now

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,28 +6,22 @@ export interface Testimonial {
 
 export const testimonials: Testimonial[] = [
   {
-    name: "Jaya Ibubapak adek2iparcndracuttrrcinta",
-    date: new Date("2025-12-22"),
+    name: "Muhammad Irfan Ardiansyah",
+    date: new Date("2026-04-22"),
     content:
-      "Masalah terselesaikan sampai. Tuntas dengan ketulusan hati beliau membantu yang lemah dan semoga selalu di lancarkan di. Banyakkan rizkinya buat Pak okto dan team . Terimaksih banyak. Salam kenal . God blessing you",
+      "Terima kasih atas dedikasinya dalam menangani kasus. Terbaik! Sukses selalu Pak Okto & Tim.",
   },
   {
-    name: "Alwin Wahyu",
-    date: new Date("2025-1-22"),
+    name: "Onny Pandika",
+    date: new Date("2026-04-21"),
     content:
-      "Sangat membantu saudara saya. Pelayanan sangat baik. Semoga semakin sukses Pengacara Surabaya ( Pak Octo dan staff ) salam sehat TYM",
+      "Sangat membantu dalam urusan hukum. Team yang ada sangat berpengalaman. Jangan ragu menghubungi beliau jika membutuhkan bantuan hukum.",
   },
   {
     name: "Mukhsin Saleh",
-    date: new Date("2025-10-22"),
+    date: new Date("2026-04-21"),
     content:
       "Pak dwi ockto saya kenal sangat baik dan tg jawab dlm hal semua kasus clainnya beliau komitmen dan kinerjanya sangat memuaskan bagi pencari keadilan kelebihan pak octo tdk orientasi ke cuan tat kala membela orang yg lemah bangga indonesia memiliki pengacara yg hebat demi membela yg hak orang2 yg terdzolimi",
-  },
-  {
-    name: "Junias Andrian",
-    date: new Date("2025-10-05"),
-    content:
-      "Pengacara sangat berpengalaman, proses dari awal hingga akhir dibantu dengan mulus dan hasilnya cepat, sangat memuaskan",
   },
   {
     name: "Livia Salim",
@@ -35,6 +29,13 @@ export const testimonials: Testimonial[] = [
     content:
       "Pak Otto, seorang pengacara yang baik dan bertggjawab, proses lancar dan sukses, terima kasih bnyk Pak Otto dan Team, sukses terus Pak Otto SBY",
   },
+  {
+    name: "Junias Andrian",
+    date: new Date("2025-10-05"),
+    content:
+      "Pengacara sangat berpengalaman, proses dari awal hingga akhir dibantu dengan mulus dan hasilnya cepat, sangat memuaskan",
+  },
+
   {
     name: "Hubert Jong",
     date: new Date("2025-12-17"),
